b797925c9d31f04f24a689e8fed517fff1ffb43d
steve-ryan <stevohyoung@gmail.com>
2022-05-02 00:01:32 +0300

    introduction to express for metorship 101

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+// instantiate an Express application by calling the express method and store it in a variable called app.
+const app = express();
+
+const port = 3000;
+
+// This creates a route for our app. The first argument is the path we want to use. The second argument is the router handler, a callback function that will be called when a request is made to the path.
+app.get("/",(req,res)=>{
+    res.send("Hello world from steve");
+});
+
+app.get("/users/:id",(req,res)=>{
+ res.send(`user ${req.params.id}`);
+});
+
+app.listen(port,()=>{
+    console.log(`App running from port ${port}`);
+});
