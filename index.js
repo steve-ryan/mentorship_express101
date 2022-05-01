be58263fcaaf7667091ee6df450bdcc76e571cab
steve-ryan <stevohyoung@gmail.com>
2022-05-02 01:08:45 +0300

    redirect

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = 3000;
 
 // This creates a route for our app. The first argument is the path we want to use. The second argument is the router handler, a callback function that will be called when a request is made to the path.
 app.get("/",(req,res)=>{
+    res.redirect('/jso');
     res.send("Hello world from steve");
+});
+//This is how to send a JSON response
+app.get("/jso",(req,res)=>{
+ res.status(200);
+ res.json({"response":"Hello babe!"});
 });
 
 app.get("/users/:id",(req,res)=>{
